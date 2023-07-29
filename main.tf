acaa78452f735b4905e4806a48aa1fdcb3136b56
TaeHyong Kim <kthstar27@gmail.com>
2023-07-29 11:16:56 +0900

    1.0

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+# Standard AWS Provider Block
+terraform {
+    required_version = ">= 1.0"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = ">= 5.0"
+        }
+    }
+}
+
 resource "null_resource" "REMOTE_EXECUTE_COMMAND" {
     count = (length(var.REMOTE_EXECUTE_COMMAND) > 0 ? 1 : 0)
 
@@ -39,24 +50,24 @@ resource "null_resource" "REMOTE_CREATE_FILE" {
     # }
 }
 
-resource "null_resource" "REMOTE_SEND_FILE" {
-    count = (length(var.RREMOTE_SEND_FILE_DATA.SOURCE) > 0 ? 1 : 0)
+# resource "null_resource" "REMOTE_SEND_FILE" {
+#     count = (length(var.REMOTE_CREATE_FILE.SOURCE) > 0 ? 1 : 0)
 
-    connection {
-        host        = "${var.REMOTE_HOST.IP}"
-        type        = "ssh"
-        user        = "${var.REMOTE_HOST.USER}"  # Update with your SSH username
-        private_key = file("${var.REMOTE_HOST.PRI_KEY_FILE}")  # Update with the path to your private key file
-    }
+#     connection {
+#         host        = "${var.REMOTE_HOST.IP}"
+#         type        = "ssh"
+#         user        = "${var.REMOTE_HOST.USER}"  # Update with your SSH username
+#         private_key = file("${var.REMOTE_HOST.PRI_KEY_FILE}")  # Update with the path to your private key file
+#     }
 
-    dynamic "provisioner" {
-        for_each = var.RREMOTE_SEND_FILE_DATA
+#     dynamic "provisioner" {
+#         for_each = var.REMOTE_CREATE_FILE
 
-        content {
-            provisioner "file" {
-                source      = provisioner.value.SOURCE
-                destination = provisioner.value.DESTINATION
-            }
-        }
-    }
-}
+#         content {
+#             provisioner "file" {
+#                 source      = provisioner.value.SOURCE
+#                 destination = provisioner.value.DESTINATION
+#             }
+#         }
+#     }
+# }
